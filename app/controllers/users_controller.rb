c9b8a6628c74ef17023f255d66000531809d319b
Jeremy Nagahama <naga@hawaii.edu>
2017-12-09 08:20:58 -1000

    Add /create form template and styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+  def show
+    #Declare a user, and set it to User.find(id), with 'id' being a number.
+    @user = User.find(params[:id])
+  end
+  def index
   end
 end
