40186733729919814fa055dac19ff558b4e958cd
Jeremy Nagahama <naga@hawaii.edu>
2017-12-10 09:17:31 -1000

    Add partial 'error_messages', which lists any error messages if a user submits with bad data.
    TODO: Add CSS to error_messages ('Note that Listing 7.21')

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,28 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def show
     #Declare a user, and set it to User.find(id), with 'id' being a number.
     @user = User.find(params[:id])
   end
+
   def index
+
+  end
+
+  def create
+    @user = User.new(user_params) # Not the final implemntation!
+    if @user.save
+      # Handle a succesful save.
+    else 
+      render 'new'
+    end
+  end
+
+  #Set params that is needed to allow User.new().
+  #Requires the user hash.
+  private def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
